a7a8c5110f7baa1ca8f0b270d0c466bd261c4e80
Sunil <sunhick@gmail.com>
2016-09-21 00:50:12 -0600

    added documentation for nareq error

--- public/javascripts/module.js
+++ public/javascripts/module.js
@@ -1,7 +1,10 @@
 /**
- *  @Author: Sunil bn <sunhick@gmail.com>
- *  @Desc: testApp module 
- *
- *  Copyright (c) 2016. University of Colorado, boulder
- */
+*  @Author: Sunil bn <sunhick@gmail.com>
+*  @Desc: testApp module 
+*
+*  Copyright (c) 2016. University of Colorado, boulder
+*/
+
+// Module should be declared as below only once. 
+// And controller can access it as angular.module('testApp').controller(....);
 var app = angular.module('testApp', []);
